10e9e9f1194980b8122a139eab687da98034f227
a8anassis <a8anassis@gmail.com>
2025-09-08 20:13:01 +0300

    feat: add I/O

---
+++ ReadApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReadApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float f = 0.0f;
+            double d = 0d;
+
+            Console.WriteLine("Παρακαλώ εισάγετε δύο δεκαδικούς");
+            f = float.Parse(Console.ReadLine()!);
+            d = double.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"f = {f:N2}\td = {d:N2}");
+        }
+    }
+}
