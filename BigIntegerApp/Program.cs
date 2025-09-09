028074661ae906f1805afd8a314f2c645313b7e7
a8anassis <a8anassis@gmail.com>
2025-09-09 19:22:38 +0300

    feat: add big integer app and demo operator overloading

---
+++ BigIntegerApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace BigIntegerApp
+{
+    /// <summary>
+    /// Υπολογίζει το γινόμενο 1*2*3*...n
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            BigInteger result = 1;  // auto-boxing και υπερφόρτωση του = για big integers
+            int i = 1;
+
+            while (i <= n)
+            {
+                result *= i;
+                i++;
+            }
+
+            Console.WriteLine($"1 * 2 * 3 * ... * {n} = {result:N0}");
+        }
+    }
+}
