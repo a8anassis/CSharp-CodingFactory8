961e788b9a13137e9b7f15fafab32d6c76b2e6ec
a8anassis <a8anassis@gmail.com>
2025-09-09 18:58:10 +0300

    feat: add conditional logic with if and ternary

---
+++ TernaryApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TernaryApp
+{
+    /// <summary>
+    /// Τριαδικός τελεστής μέσα σε WriteLine.
+    /// Επσιτρέφει το απόλυτο ενός ακεράιου που δίνει
+    /// ο χρήστης.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Παρακαλώ εισάγετε ένα ακέραιο");
+
+            if (!int.TryParse(Console.ReadLine(), out int inputNum))
+            {
+                Console.WriteLine("Λάθος κατά την εισαγωγή");
+                return;
+            }
+
+            Console.WriteLine("Το απόλυτο του {0} = {1}", inputNum, (inputNum >= 0) ? inputNum : -inputNum);
+        }
+    }
+}
