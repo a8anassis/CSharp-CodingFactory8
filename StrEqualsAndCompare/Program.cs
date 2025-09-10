251f02d784c22b307eb5f7113e65d4e0a769f1bf
a8anassis <a8anassis@gmail.com>
2025-09-10 19:11:51 +0300

    feat: string methods

---
+++ StrEqualsAndCompare/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StrEqualsAndCompare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "AUEB";
+            string s2 = "AUEB";
+
+            Console.WriteLine($"{s1.Equals(s2)}");  // ισότητα περιεχομένου
+            Console.WriteLine($"{s1 == s2}");       // ισότητα περιεχομένου
+            Console.WriteLine($"{ReferenceEquals(s1, s2)}"); // ισότητα αναφοράς
+
+            int result = s1.CompareTo(s2); // λεξικογραφική σύγκριση
+            int result2 = string.Compare("HELLO", "hello", true); // λεξικογραφική σύγκριση -- null safe
+        }
+    }
+}
---
+++ StringApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StringApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = null;                      // non-nullable, 
+                                // nullable
+            string s3 = "AUEB";         // immutable
+            string s4 = "AUEB";         // intering
+            string s5 = new string("AUEB");
+
+
+        }
+    }
+}
