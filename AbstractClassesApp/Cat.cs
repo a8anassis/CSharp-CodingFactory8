2311d09c3861e4da9fb9c62d0e7737b0bf7c8b27
a8anassis <a8anassis@gmail.com>
2025-09-16 20:26:31 +0300

    feat: add abstract classes

---
+++ AbstractClassesApp/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesApp
+{
+    internal class Cat : AbstractAnimal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Cat says hello.");
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Age: {Age}";
+        }
+    }
+}
