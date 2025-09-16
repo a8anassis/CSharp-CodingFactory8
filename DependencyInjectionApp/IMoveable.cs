535e4ec6b9fee6a9aa0b5fe50c09b0915f0a4737
a8anassis <a8anassis@gmail.com>
2025-09-16 21:00:54 +0300

    feat: add DI

---
+++ DependencyInjectionApp/IMoveable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesApp
+{
+    internal interface IMoveable
+    {
+        void Move5();
+
+        // Default Impl
+        void Move10()
+        {
+            Console.WriteLine("Move 10 units");
+        }
+    }
+}
---
+++ DependencyInjectionApp/MovingSpace.cs
@@ -0,0 +1,24 @@
+﻿using InterfacesApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionApp
+{
+    internal class MovingSpace
+    {
+        public IMoveable? Moveable { get; }
+
+        public MovingSpace(IMoveable? moveable)
+        {
+            Moveable = moveable;
+        }
+
+        public void MakeMove5()
+        {
+            Moveable!.Move5();
+        }
+    }
+}
