6619ca7d5c3022537042d6d830e6c7a21fbc9d85
a8anassis <a8anassis@gmail.com>
2025-09-16 20:07:45 +0300

    feat: add polymorphism

--- PointApp/Program.cs
+++ PointApp/Program.cs
@@ -11,5 +11,17 @@ namespace PointApp
             Point3D p3 = new() { X = 10, Y = 20, Z = 30 };
             Point p4 = new Point3D() { X = 100, Y = 200, Z = 300 }; // Type polymorphism
         }
+
+        // Πολυμορφική γιατί η παράμετρος είναι πολυμορφική
+        // @runtime οτιδήποτε IS-A Point
+        public static void DoMove5(Point p)
+        {
+            p.Move5();
+        }
+
+        public static void DoPrint(Point p)
+        {
+            Console.WriteLine(p);
+        }
     }
 }
