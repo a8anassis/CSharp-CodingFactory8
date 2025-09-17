e43d1c957a8a7efad61acd9d71a0fd5bf408e4e5
a8anassis <a8anassis@gmail.com>
2025-09-17 21:00:23 +0300

    feat: add file management app

--- FileManagement/Program.cs
+++ FileManagement/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace FileManagement
+﻿using FileManagement.Model;
+
+namespace FileManagement
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DLList<char> fileList = new();
+            GenericNode<char>? node;
+            int totalChars = 0;
+            string filePath = @"C:\tmp\file.txt";
+            char ch;
+            int ordinal;
+
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine($"File not found: {filePath}");
+                    return;
+                }
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    while ((ordinal = reader.Read()) != -1)
+                    {
+                        ch = (char) ordinal;
+                        if (ch is '\r' or '\n') continue;
+
+                        node = fileList.FindNode(ch);
+                        if (node == null)
+                        {
+                            fileList.InsertLast(ch);
+                        }
+                        else
+                        {
+                            fileList.IncreaseCount(ch);
+                        }
+                        totalChars++;
+                    }   
+                }
+                fileList.SortByCount();
+                fileList.Traverse(totalChars);
+            }
+            catch (IOException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
